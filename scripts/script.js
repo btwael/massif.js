ec72d92d471c26ba0f644cf410336bddab01a5bc
Wael Boutglay <btwael@gmail.com>
2018-03-25 23:15:48 +0100

    Release 0.0.1

--- scripts/script.js
+++ scripts/script.js
@@ -13,7 +13,6 @@ application.controller("MassifVisualizeController", [function() {
     // init
     var scope = this;
     scope.source = "";
-    var chart = document.getElementById("chart").getContext('2d');
     // events
     scope.process = function() {
         var parsed = Massifjs.parse(scope.source);
@@ -32,16 +31,19 @@ application.controller("MassifVisualizeController", [function() {
             data.labels.push(largestTime = snapshot.time);
             data.datasets[0].data.push(largestMemory = (snapshot.mem_heap_B + snapshot.mem_heap_extra_B + snapshot.mem_stacks_B));
         });
-        var scale = rightUnit(1024, largestMemory);
-        data.datasets[0].label = parsed.cmd + " (" + scale[1] + "B)";
+        var munit = rightUnit(1024, largestMemory);
+        data.datasets[0].label = parsed.cmd;
         data.datasets[0].data.forEach(function(bytes, i) {
-            data.datasets[0].data[i] = (data.datasets[0].data[i] / scale[0]).toFixed(3);
+            data.datasets[0].data[i] = (data.datasets[0].data[i] / munit[0]).toFixed(3);
         });
-        scale = rightUnit(1000, largestTime);
+        var mtime = rightUnit(1000, largestTime);
         data.labels.forEach(function(time, i) {
-            data.labels[i] = (data.labels[i] / scale[0]).toFixed(0);
+            data.labels[i] = (data.labels[i] / mtime[0]).toFixed(0);
         });
         console.log(data.datasets[0].data[data.datasets[0].data.length - 1]);
+        document.getElementById("chart").remove(); // this is my <canvas> element
+        document.getElementById("chart-container").innerHTML += '<canvas class="col-12" id="chart"></canvas>';
+        var chart = document.getElementById("chart").getContext('2d');
         new Chart(chart, {
             type: 'line',
             data: data,
@@ -49,8 +51,23 @@ application.controller("MassifVisualizeController", [function() {
                 scales: {
                     xAxes: [
                         {
+                            scaleLabel: {
+                                display: true,
+                                labelString: "Time (" + (munit[1] == "K" ? "10^3 " : munit[1] == "M" ? "10^6 " : munit[1] == "G" ? "10^9 " : "") + "instructions)"
+                            },
+                        }
+                    ],
+                    yAxes: [
+                        {
+                            scaleLabel: {
+                                display: true,
+                                labelString: "Memory consumption (" + munit[1] + "B)"
+                            },
                             ticks: {
-                                stepSize: parsed.snapshots.length
+                                stepSize: parsed.snapshots.length,
+                                callback: function(value) {
+                                    return value + " " + munit[1] + "B";
+                                }
                             }
                         }
                     ]
